a8ae12850c8ec16bfd1f8331379a650b2685838a
kaloyaan <42871424+kaloyaan@users.noreply.github.com>
2018-09-07 11:56:11 +0300

    Update templater.sh

--- templater.sh
+++ templater.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-source bash-templater-hw/vars.txt
+source ./vars.txt
 
-cat bash-templater-hw/template.txt | \
+cat ./template.txt | \
 sed "s/@REPLACE1@/$REPLACE1/g;s/@REPLACE2@/$REPLACE2/g;s/@REPLACE3@/$REPLACE3/g;\
 s/@REPLACE4@/$REPLACE4/g;s/@REPLACE5@/$REPLACE5/g;s/@REPLACE6@/$REPLACE6/g;\
 s/@REPLACE7@/$REPLACE7/g;s/@REPLACE8@/$REPLACE8/g;s/@REPLACE9@/$REPLACE9/g;" | \
-cat > bash-templater-hw/result.txt
+cat > ./result.txt
